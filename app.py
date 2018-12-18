48594e436b4f0de634694992c0d5a6dc87f3134a
Walter Obrian <water_obrian@foxmail.com>
2018-12-18 09:35:36 +0800

    undo

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder="static/dist", template_folder="static")
 
 
-asdasdasdasdasd
 @app.route("/")
 def index():
     return render_template("index.html")
